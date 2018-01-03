b94864ab21a38dc84ea94cfdd93223abab408ac9
bouzuya <m@bouzuya.net>
2018-01-04 06:41:03 +0900

    extract comparePixels

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import {
   newImage,
   setPixel
 } from './data/image';
-import { isSamePixel, newPixel } from './data/pixel';
+import { Pixel, isSamePixel, newPixel } from './data/pixel';
 import {
   Result,
   getDiffDimension,
@@ -19,6 +19,12 @@ import {
   newSameResult
 } from './data/result';
 
+const comparePixels = (p1: Pixel, p2: Pixel): [boolean, Pixel] => {
+  return isSamePixel(p1, p2)
+    ? [true, p1] // p1 = p2
+    : [false, newPixel(0xff, 0x00, 0xff, 0xff)];
+};
+
 const compareImages = (
   image1: Image,
   image2: Image
@@ -26,7 +32,6 @@ const compareImages = (
   if (!isSameDimension(image1, image2)) {
     return newNotSameDimensionResult(image1, image2);
   }
-  const errorPixel = newPixel(0xff, 0x00, 0xff, 0xff);
   const height = image1.height; // = image2.height
   const width = image1.width; // = image2.width
   const diffImage = newBlankImage(height, width);
@@ -35,12 +40,9 @@ const compareImages = (
     for (let x = 0; x < width; x++) {
       const p1 = getPixel(image1, x, y);
       const p2 = getPixel(image2, x, y);
-      if (isSamePixel(p1, p2)) {
-        setPixel(diffImage, x, y, p1); // = p2
-      } else {
-        setPixel(diffImage, x, y, errorPixel);
-        isSame = false;
-      }
+      const [b, p] = comparePixels(p1, p2);
+      setPixel(diffImage, x, y, p);
+      isSame = isSame && b;
     }
   }
   return isSame ? newSameResult() : newNotSameResult(diffImage);
