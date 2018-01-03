3b2b77e286f46b9940dfac2a8bbd7676e38132c0
bouzuya <m@bouzuya.net>
2018-01-03 18:41:42 +0900

    add -> compareImages

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import { Image } from './type/image';
 
-const add = (x: number, y: number): number => x + y;
+const compareImages = (
+  image1: Image,
+  image2: Image
+): boolean => {
+  return image1.height === image2.height && image1.width === image2.width;
+};
 
-export { Image, add };
+export { Image, compareImages };
--- test/index.ts
+++ test/index.ts
@@ -1,21 +1,29 @@
 import { Test, run, test } from 'beater';
 import * as assert from 'power-assert';
-import { Image, add } from '../src';
+import { Image, compareImages } from '../src';
 
-const newImage = (): Image => {
-  const data = new Buffer(4 * 1);
-  data[0] = 0x00;
-  data[1] = 0x00;
-  data[2] = 0x00;
-  data[3] = 0x00;
-  return { data, height: 1, width: 1 };
+const newImage = (height: number, width: number): Image => {
+  const data = new Buffer(4 * width * height);
+  data.fill(0x00);
+  return { data, height, width };
 };
 
+const category = 'compareImages ';
 const tests: Test[] = [
-  test('add', () => {
-    const d = newImage();
-    assert(d.width === d.width);
-    assert(add(1, 2) === 3);
+  test(category + 'not same dimention (width)', () => {
+    const image1 = newImage(1, 1);
+    const image2 = newImage(1, 2);
+    assert(compareImages(image1, image2) === false);
+  }),
+  test(category + 'not same dimention (height)', () => {
+    const image1 = newImage(1, 1);
+    const image2 = newImage(2, 1);
+    assert(compareImages(image1, image2) === false);
+  }),
+  test(category + 'same dimention', () => {
+    const image1 = newImage(1, 1);
+    const image2 = newImage(1, 1);
+    assert(compareImages(image1, image2) === true);
   })
 ];
 
